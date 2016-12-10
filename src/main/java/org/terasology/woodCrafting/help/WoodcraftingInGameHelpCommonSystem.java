cd75303574d64e05c515695db5e1cb7ac6d74bbe
XTariq <xrtariq@gmail.com>
2016-12-09 17:14:37 -0700

    Moved some items around.

--- src/main/java/org/terasology/woodCrafting/help/WoodcraftingInGameHelpCommonSystem.java
+++ src/main/java/org/terasology/woodCrafting/help/WoodcraftingInGameHelpCommonSystem.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terasology.woodCrafting;
+package org.terasology.woodCrafting.help;
 
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
